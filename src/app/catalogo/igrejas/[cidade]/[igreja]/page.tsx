efd252859dc40f77ed96dcbd6ca591e9080eb6a2
Vinicius Magalhaes <viniciusmagalhaes.vsm@gmail.com>
2025-04-08 05:05:39 -0300

    fix: remove prop sticky do AppHeader na página de eventos da igreja

--- src/app/catalogo/igrejas/[cidade]/[igreja]/page.tsx
+++ src/app/catalogo/igrejas/[cidade]/[igreja]/page.tsx
@@ -156,7 +156,6 @@ export default function CatalogoEventosIgrejaPage({
         subtitle={`Eventos em ${formatarCidade(cidade)}`}
         showBackButton={true}
         backUrl="/catalogo/igrejas"
-        sticky={true}
       />
 
       {/* Conteúdo principal */}
